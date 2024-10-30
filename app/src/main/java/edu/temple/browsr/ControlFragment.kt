78151c198e654ccf4ea713de054c2364ce28dd14
bictorg <vj.jimeneznunez@gmail.com>
2024-10-30 13:24:02 -0400

    Impl. control fragment logic

---
+++ app/src/main/java/edu/temple/browsr/ControlFragment.kt
@@ -0,0 +1,44 @@
+class ControlFragment : Fragment() {
+    private lateinit var urlEditText: EditText
+    private lateinit var goButton: ImageView
+    private lateinit var backButton: ImageView
+    private lateinit var forwardButton: ImageView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_control, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        urlEditText = view.findViewById(R.id.urlEditText)
+        goButton = view.findViewById(R.id.goButton)
+        backButton = view.findViewById(R.id.backButton)
+        forwardButton = view.findViewById(R.id.forwardButton)
+
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        goButton.setOnClickListener {
+            val url = urlEditText.text.toString()
+            (activity as? MainActivity)?.loadUrl(url)
+        }
+
+        backButton.setOnClickListener {
+            (activity as? MainActivity)?.goBack()
+        }
+
+        forwardButton.setOnClickListener {
+            (activity as? MainActivity)?.goForward()
+        }
+    }
+
+    fun updateUrl(url: String) {
+        urlEditText.setText(url)
+    }
+} 
